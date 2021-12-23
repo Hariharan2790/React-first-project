c0a39fe8bf18e9af6e71a7aac7b6dc9588b3a65c
hariharan <hariharan.mct@gmail.com>
2021-12-23 13:52:57 +0530

    Pricing Cart React

--- src/App.js
+++ src/App.js
@@ -1,24 +1,151 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Pricecard from './Pricecard';
+
 
 function App() {
+  let priceList = [
+    {
+      plan: "FREE",
+      price: "0",
+      currency: "$",
+      period: "month",
+      features: [
+        {
+          name: "Single User",
+          enable: true
+        },
+        {
+          name: "5GB Storage",
+          enable: true
+        },
+        {
+          name: "Unlimited Public Projects",
+          enable: true
+        },
+        {
+          name: "Community Access",
+          enable: true
+        },
+        {
+          name: "Unlimited Private Projects",
+          enable: false
+        },
+        {
+          name: "Dedicated Phone Support",
+          enable: false
+        },
+        {
+          name: "Free Subdomain",
+          enable: false
+        },
+        {
+          name: "Monthly Status Reports",
+          enable: false
+        }
+
+      ]
+
+    },
+    {
+      plan: "PLUS",
+      price: "9",
+      currency: "$",
+      period: "month",
+      features: [
+        {
+          name: "5 Users",
+          enable: true,
+          isStrong : true
+        },
+        {
+          name: "50GB Storage",
+          enable: true
+        },
+        {
+          name: "Unlimited Public Projects",
+          enable: true
+        },
+        {
+          name: "Community Access",
+          enable: true
+        },
+        {
+          name: "Unlimited Private Projects",
+          enable: true
+        },
+        {
+          name: "Dedicated Phone Support",
+          enable: true
+        },
+        {
+          name: "Free Subdomain",
+          enable: true
+        },
+        {
+          name: "Monthly Status Reports",
+          enable: false
+        }
+
+      ]
+
+
+    },
+    {
+      plan: "PRO",
+      price: "49",
+      currency: "$",
+      period: "month",
+      features: [
+        {
+          name: "Unlimited Users",
+          enable: true,
+          isStrong : true
+        },
+        {
+          name: "150GB Storage",
+          enable: true
+        },
+        {
+          name: "Unlimited Public Projects",
+          enable: true
+        },
+        {
+          name: "Community Access",
+          enable: true
+        },
+        {
+          name: "Unlimited Private Projects",
+          enable: true
+        },
+        {
+          name: "Dedicated Phone Support",
+          enable: true
+        },
+        {
+          name: "Free Subdomain",
+          enable: true
+        },
+        {
+          name: "Monthly Status Reports",
+          enable: true
+        }
+
+      ]
+
+
+    }
+  ]
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section class="pricing py-5">
+      <div class="container">
+        <div class="row">
+          <Pricecard data = {priceList[0]}></Pricecard>
+          <Pricecard data = {priceList[1]}></Pricecard>
+          <Pricecard data = {priceList[2]}></Pricecard>
+        </div>
+      </div>
+    </section>
   );
 }
 
